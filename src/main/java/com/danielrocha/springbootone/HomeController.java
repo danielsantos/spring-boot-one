723e0f48b9193369df29ab4f413e4f04454b79c3
Daniel Rocha <daniel.rocha@riocard.com>
2018-10-29 10:50:24 -0300

    first commit

---
+++ src/main/java/com/danielrocha/springbootone/HomeController.java
@@ -0,0 +1,36 @@
+package com.danielrocha.springbootone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@GetMapping("/")
+	public ModelAndView index() {
+		
+		ModelAndView mv = new ModelAndView("index");
+		
+		mv.addObject("listaUsuarios", usuarioRepository.findAll());
+		mv.addObject("usuario", new Usuario());
+		
+		return mv;
+		
+	}
+	
+	@PostMapping("/save")
+	public ModelAndView save(Usuario usuario) {
+		
+		usuarioRepository.save(usuario);
+		
+		return index();
+		
+	}
+	
+}
